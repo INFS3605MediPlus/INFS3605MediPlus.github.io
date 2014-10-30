b40f661c2a01a93c3da002d98458c82fb37eaddf
Devesh Mitra <mitra.devesh@gmail.com>
2014-10-31 00:56:15 +1100

    Updating to latest version
    Updating to latest version

--- js/search.js
+++ js/search.js
@@ -18,27 +18,38 @@ searchonload = function(){
         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
     }
 
-    function searchPatientParse() {        
-        var Patient = Parse.Object.extend("Patient");  
-        var query = new Parse.Query(Patient);
+    function searchPatientParse() {
+        var Patient = Parse.Object.extend("Patient");
+        var queryFirstname = new Parse.Query(Patient);
+        var queryLastname = new Parse.Query(Patient);
+        var queryMedicare = new Parse.Query(Patient);
         // NOTE: we may have to uppercase the first character before passing it into the query!
-        var patientFirstName = getURLParameter("firstName");
-        var patientLastName = getURLParameter("lastName");
-        if (patientFirstName == null) patientFirstName = "";
-        if (patientLastName == null) patientLastName = "";
-        query.startsWith("First_Name", patientFirstName);
-        query.startsWith("Last_Name", patientLastName);
-        query.limit(10);
-        query.find({
+        var queryVal = capitaliseFirstLetter(getURLParameter("q"));
+        if (queryVal == null) queryVal = "";
+        
+        queryFirstname.startsWith("First_Name", queryVal);
+        queryFirstname.limit(5);
+        queryLastname.startsWith("Last_Name", queryVal);
+        queryLastname.limit(5);
+        
+        if (!isNaN(queryVal)) {
+            queryVal = Number(queryVal);
+        }
+        queryMedicare.equalTo("Medicare_No", queryVal);
+        queryMedicare.limit(1);
+        
+        var mainQuery = Parse.Query.or(queryFirstname, queryLastname, queryMedicare);
+        mainQuery.find({
             success: function(results){
                 for (var i=0; i<results.length; i++){
                     var object = results[i];
                     
-                    $("#search-results-table").append("<tr><td>" + i + "</td><td><a href='patient.html?patientID=" + object.id + "'>" + object.get('First_Name') + "</a></td><td>" + object.get('Last_Name') + "</td><td>" + object.get('Contact_No') + "</td></tr>");
-                }          
+                    $("#search-results-table").append("<tr><td>" + i + "</td><td><a class='patient-result' href='patient.html?patientID=" + object.id + "' data-toggle='tooltip' data-placement='right' title='See patient details'>" + object.get('First_Name') + "</a></td><td>" + object.get('Last_Name') + "</td><td>" + object.get('Medicare_No') + "</td></tr>");
+                }
+                $('.patient-result').tooltip();
             },
             error: function(error){
-                alert("No Patients Found");
+                alert(error.message);
             }
         });       
     }
